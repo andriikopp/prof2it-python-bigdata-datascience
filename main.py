5103a76b48d8a70dcf7e13419a00f99050d8c398
Andrii Kopp <kopp93@gmail.com>
2023-05-02 20:59:50 +0300

    Fix tasks 7-8.

--- main.py
+++ main.py
@@ -4,89 +4,155 @@ from pyspark.sql import SparkSession
 def get_ukrainian_series_movies_titles(spark):
     """1. Get all titles of series/movies etc. that are available in Ukrainian."""
     return spark.sql("""
-                     SELECT DISTINCT title 
-                     FROM title_akas 
-                     WHERE region = 'UA'
-                     """)
+                        SELECT 
+                            DISTINCT title
+                        FROM 
+                            title_akas
+                        WHERE 
+                            region = 'UA'
+                        """)
 
 
 def get_people_born_in_19th_century(spark):
     """2. Get the list of people’s names, who were born in the 19th century."""
     return spark.sql("""
-                     SELECT primaryName 
-                     FROM name_basics 
-                     WHERE birthYear >= 1801 AND birthYear <= 1900
-                     """)
+                        SELECT 
+                            primaryName
+                        FROM 
+                            name_basics
+                        WHERE 
+                            birthYear >= 1801 AND birthYear <= 1900
+                        """)
 
 
 def get_movies_last_more_2_hours(spark):
     """3. Get titles of all movies that last more than 2 hours."""
     return spark.sql("""
-                     SELECT primaryTitle 
-                     FROM title_basics 
-                     WHERE titleType = 'movie' AND runtimeMinutes > 120
-                     """)
+                        SELECT 
+                            primaryTitle
+                        FROM 
+                            title_basics
+                        WHERE 
+                            titleType = 'movie' AND runtimeMinutes > 120
+                        """)
 
 
 def get_people_and_characters_played(spark):
     """4. Get names of people, corresponding movies/series and characters they played in those films."""
     return spark.sql("""
-                     SELECT name_basics.primaryName, title_basics.primaryTitle, title_principals.characters 
-                     FROM (title_basics INNER JOIN title_principals ON title_basics.tconst = title_principals.tconst) 
-                        INNER JOIN name_basics ON name_basics.nconst = title_principals.nconst 
-                     WHERE title_principals.category IN ('actor', 'actress')
-                     """)
+                        SELECT 
+                            name_basics.primaryName, 
+                            title_basics.primaryTitle,
+                            title_principals.characters
+                        FROM 
+                            (title_basics INNER JOIN title_principals ON title_basics.tconst = title_principals.tconst) 
+                                INNER JOIN name_basics ON name_basics.nconst = title_principals.nconst
+                        WHERE 
+                            title_principals.category IN ('actor', 'actress')
+                        """)
 
 
 def get_top_100_adult_movies_per_region(spark):
     """5. Get information about how many adult movies/series etc. there are per region. 
     Get the top 100 of them from the region with the biggest count to the region with the smallest one."""
-    return spark.sql("""
-                     SELECT title_akas.region, COUNT(title_basics.tconst) AS total 
-                     FROM title_akas INNER JOIN title_basics ON title_basics.tconst = title_akas.titleId 
-                     WHERE title_basics.isAdult = 1 
-                     GROUP BY title_akas.region 
-                     ORDER BY total DESC 
-                     LIMIT 100
-                     """)
+    return spark.sql(r"""
+                        SELECT 
+                            title_akas.region, 
+                            COUNT(title_basics.tconst) AS total
+                        FROM 
+                            title_akas INNER JOIN title_basics ON title_basics.tconst = title_akas.titleId
+                        WHERE 
+                            title_basics.isAdult = 1 AND title_akas.region != '\N'
+                        GROUP BY 
+                            title_akas.region
+                        ORDER BY 
+                            total DESC
+                        LIMIT 100
+                        """)
 
 
 def get_top_50_tvseries_by_episodes(spark):
     """6. Get information about how many episodes in each TV Series. Get the top 50 of them starting from the TV Series with the biggest quantity of episodes."""
     return spark.sql("""
-                     SELECT title_basics.primaryTitle, COUNT(title_episode.tconst) as episodes
-                     FROM title_basics INNER JOIN title_episode ON title_basics.tconst = title_episode.parentTconst
-                     WHERE title_basics.titleType = 'tvseries'
-                     GROUP BY title_basics.primaryTitle
-                     ORDER BY episodes DESC
-                     LIMIT 50
-                     """)
+                        SELECT 
+                            title_basics.primaryTitle, 
+                            COUNT(title_episode.tconst) as episodes
+                        FROM 
+                            title_basics INNER JOIN title_episode ON title_basics.tconst = title_episode.parentTconst
+                        GROUP BY 
+                            title_basics.primaryTitle
+                        ORDER BY 
+                            episodes DESC
+                        LIMIT 50
+                        """)
 
 
 def get_10_most_popular_titles_by_each_decade(spark):
     """7. Get 10 titles of the most popular movies/series etc. by each decade."""
+    decades_df = spark.sql("""
+                        SELECT
+                            CONCAT(
+                                FLOOR(title_basics.startYear / 10) * 10, 
+                                ' - ', 
+                                FLOOR(title_basics.startYear / 10) * 10 + 9
+                            ) as decade, 
+                            title_basics.primaryTitle, 
+                            title_ratings.averageRating
+                        FROM
+                            title_basics INNER JOIN title_ratings ON title_basics.tconst = title_ratings.tconst
+                        """)
+    decades_df.createOrReplaceTempView("decades_view")
     return spark.sql("""
-                     SELECT *
-                     FROM (
-                         SELECT CONCAT(FLOOR(startYear / 10) * 10, ' - ', FLOOR(startYear / 10) * 10 + 9) as decade, title_basics.primaryTitle, title_ratings.averageRating, ROW_NUMBER() OVER (PARTITION BY title_basics.primaryTitle ORDER BY title_ratings.averageRating DESC) AS num_titles
-                         FROM title_basics INNER JOIN title_ratings ON title_basics.tconst = title_ratings.tconst
-                     ) AS sub_query
-                     WHERE num_titles == 10
-                     ORDER BY decade DESC, averageRating DESC
-                     """)
+                        SELECT *
+                        FROM (
+                            SELECT 
+                                decade, 
+                                primaryTitle, 
+                                averageRating, 
+                                ROW_NUMBER() OVER (
+                                    PARTITION BY decade ORDER BY averageRating DESC
+                                ) AS row_num
+                            FROM 
+                                decades_view
+                        ) AS sub_query
+                        WHERE 
+                            row_num <= 10
+                        ORDER BY 
+                            decade DESC, averageRating DESC
+                        """)
 
 
 def get_10_most_popular_titles_by_each_genre(spark):
     """8. Get 10 titles of the most popular movies/series etc. by each genre."""
-    return spark.sql("""
-                     SELECT *
-                     FROM (
-                         SELECT SPLIT(title_basics.genres, ',') as genre, title_basics.primaryTitle, title_ratings.averageRating, ROW_NUMBER() OVER (PARTITION BY title_basics.primaryTitle ORDER BY title_ratings.averageRating DESC) AS num_titles
-                         FROM title_basics INNER JOIN title_ratings ON title_basics.tconst = title_ratings.tconst
-                     ) AS sub_query
-                     WHERE num_titles == 10
-                     ORDER BY genre DESC, averageRating DESC
-                     """)
+    genres_df = spark.sql("""
+                        SELECT
+                            POSEXPLODE(SPLIT(title_basics.genres, ',')) AS (pos, genre), 
+                            title_basics.primaryTitle, 
+                            title_ratings.averageRating
+                        FROM
+                            title_basics INNER JOIN title_ratings ON title_basics.tconst = title_ratings.tconst
+                        """)
+    genres_df.createOrReplaceTempView("genres_view")
+    return spark.sql(r"""
+                        SELECT *
+                        FROM (
+                            SELECT 
+                                genre, 
+                                primaryTitle, 
+                                averageRating, 
+                                ROW_NUMBER() OVER (
+                                    PARTITION BY genre ORDER BY averageRating DESC
+                                ) AS row_num
+                            FROM
+                                genres_view
+                            WHERE
+                                genre != '\N'
+                        ) AS sub_query
+                        WHERE 
+                            row_num <= 10
+                        ORDER BY 
+                            genre DESC, averageRating DESC
+                        """)
 
 
 def main():
@@ -108,7 +174,7 @@ def main():
     spark = SparkSession.builder.master("local[*]").appName("IMDB").getOrCreate()
 
     # Extraction
-    for name, file in data_sets.keys():
+    for name, file in data_sets.items():
         data_frame = spark.read.csv(input_folder + file, header="True", sep="\t")
         data_frame.createOrReplaceTempView(name)
         
